9249fd19d83d6f92d4a6f1231166dcd3f8691024
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2023-11-23 18:57:13 +0800

    1

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,25 +1,9 @@
-// checkToken();
-// async function checkToken(){
-//     let token = localStorage.getItem(`loginToken`);
-//     if(token){
-//         var response = await fetch(`https://8643-61-222-180-215.ngrok-free.app/api/Login/` + token, {
-//             method: "get",
-//             headers: new Headers({
-//               "ngrok-skip-browser-warning": "69420",
-//             }),
-//           });
-//         var responseData = await response.json();
-//         if(responseData.status != 0){
-//             openLogin();
-//         }
-//     }
-//     else{
-//         openLogin();
-//     }
-// }
-
 function openLogin(){
-    let loginUrl = "https://alan-ddddd.github.io/NewProject/html/auth-login-basic.html";
+    let loginUrl = fronturl + "/auth-login-basic.html";
     localStorage.setItem("backUrl",location.href);
     open(loginUrl,"_self");
+}
+
+function logout(){
+    
 }
---
+++ assets/js/maindata.js
@@ -0,0 +1,3 @@
+var url = "https://3ba4-2001-b011-3817-fbb8-5028-be43-b592-e82e.ngrok-free.app";
+var fronturl = "https://alan-ddddd.github.io/JD/html";
+var datalist;
