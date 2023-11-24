f22d141a1d74e4f1b207142f3f1c4151a649dd69
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2023-11-24 10:40:20 +0800

    1

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,5 +1,16 @@
+
+$(function (){
+    curruntid = localStorage.getItem("currid");
+    curruntuser = localStorage.getItem("curruser");
+    curruntlevel = localStorage.getItem("currlevel");
+
+    if(!curruntid){
+        openLogin();
+    }
+})
+
 function openLogin(){
-    let loginUrl = fronturl + "/auth-login-basic.html";
+    let loginUrl = fronturl + "/login.html";
     localStorage.setItem("backUrl",location.href);
     open(loginUrl,"_self");
 }
--- assets/js/maindata.js
+++ assets/js/maindata.js
@@ -1,3 +1,7 @@
-var url = "https://3ba4-2001-b011-3817-fbb8-5028-be43-b592-e82e.ngrok-free.app";
+//var url = "https://3ba4-2001-b011-3817-fbb8-5028-be43-b592-e82e.ngrok-free.app";
+var url = "https://localhost:7226";
 var fronturl = "https://alan-ddddd.github.io/JD/html";
 var datalist;
+var curruntuser;
+var curruntid;
+var curruntlevel;
