3f5aeca006a217729dcfd7b72039092b23526d53
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2023-11-24 14:33:54 +0800

    1

--- assets/js/maindata.js
+++ assets/js/maindata.js
@@ -1,5 +1,5 @@
-//var url = "https://3ba4-2001-b011-3817-fbb8-5028-be43-b592-e82e.ngrok-free.app";
-var url = "https://localhost:7226";
+var url = "https://9719-1-162-48-103.ngrok-free.app";
+//var url = "https://localhost:7226";
 var fronturl = "https://alan-ddddd.github.io/JD/html";
 var datalist;
 var curruntuser;
