723f933f1e79862f0575ae9cd0bec8efd99c7b22
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2023-11-27 15:08:21 +0800

    1

--- assets/js/budget.js
+++ assets/js/budget.js
@@ -1,6 +1,6 @@
 getSelfData();
 async function getSelfData(){
-  $(`#empltbody`).empty();
+  $(`#budgettbody`).empty();
     var response = await fetch(url + "/api/Budget?user=" + localStorage.getItem(`currid`), {
         method: "get",
         headers: new Headers({
@@ -9,20 +9,18 @@ async function getSelfData(){
       });
     var body = await response.json();
     console.log(body);
-    var table = $(`#empltbody`);
+    var table = $(`#budgettbody`);
     if(body.Status){
       datalist = body.Data
       $.each(datalist,function(index,data){
           table.append(`<tr>
-                            <td>${data.EMPNAME}</td>
-                            <td>${data.PHONE}</td>
-                            <td>${data.EMAIL}</td>
-                            <td>${data.ELNAME}</td>
+                            <td>${data.empname}</td>
+                            <td>${data.budget}</td>
                             <td>
                               <div class="dropdown">
-                                <button type="button" class="btn btn-primary btne" 
+                                <button type="button" class="btn btn-primary btne"
                                  data-bs-toggle="modal"
-                                 data-bs-target="#modalCenter" data-id="${data.EMPID}">修改</button>
+                                 data-bs-target="#modalCenter" data-id="${data.empid}">修改</button>
                               </div>
                             </td>
                         </tr>`);
