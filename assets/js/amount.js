7483ed117b42668d59dcca3c6dfaff6347dc9e80
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2023-11-27 21:03:52 +0800

    1

--- assets/js/amount.js
+++ assets/js/amount.js
@@ -65,7 +65,7 @@ async function getSelfData(){
       datalist = body.Data
       $.each(datalist,function(index,data){
           table.append(`<tr>
-                            ${data.payment == "新增" ? `<td style="color:green">`:`<td style="color:red">`}${data.payment}</td>
+                            ${data.payment == "新增" ? `<td style="color:green">`:`<td style="color:red">`}${data.pname}</td>
                             <td>${data.amount.numberFormat(0,".",",")}</td>
                             <td>${data.memo}</td>
                             <td>${data.u_sysdt}</td>
