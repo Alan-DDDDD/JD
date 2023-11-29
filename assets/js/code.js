2f3b9618e4a8e3d0aa8e56da5daa445048c0e3e6
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2023-11-29 20:09:09 +0800

    1

--- assets/js/code.js
+++ assets/js/code.js
@@ -84,32 +84,32 @@ $(`#systbody`).on(`click`,`.btne`,async function(){
         pgele.append(`<option value="${item.dataid}">${item.data}</option>`);
       });
     }
-  }
-  //取得細節資料
-  //getDetail($(`#sysid`).html());
-  let tbody = $(`#ddatetable tbody`);
-  tbody.empty();
-  let response = await fetch(url+"/api/Code?datagroup=" + $(`#sysid`).html(),{
-    method : "get",
-    headers : new Headers({
-      "ngrok-skip-browser-warning": "69420",
-    })
-  })
-  let datas = await response.json();
-  if(datas.Status){
-    $.each(datas.Data,(index,data)=>{
-      tbody.append(`<tr>
-      <td style="width:30%">${data.dataid}</td>
-      <td style="width:40%">${data.data}</td>
-      <td style="width:30%">
-        <small class="badge bg-label-warning dataedit" style="cursor:pointer;">編輯</small>
-        <small class="badge bg-label-danger datadel" style="cursor:pointer;">刪除</small>
-      </td>
-      </tr>`)
-    });
-  }
-  if(pg != null){
+  }if(pg != null){
     $(`#parentgroup`).change();
+  }else{
+    //取得細節資料
+    getDetail($(`#sysid`).html());
+    //  let tbody = $(`#ddatetable tbody`);
+    //  tbody.empty();
+    //  let response = await fetch(url+"/api/Code?datagroup=" + $(`#sysid`).html(),{
+    //    method : "get",
+    //    headers : new Headers({
+    //      "ngrok-skip-browser-warning": "69420",
+    //    })
+    //  })
+    //  let datas = await response.json();
+    //  if(datas.Status){
+    //    $.each(datas.Data,(index,data)=>{
+    //      tbody.append(`<tr>
+    //      <td style="width:30%">${data.dataid}</td>
+    //      <td style="width:40%">${data.data}</td>
+    //      <td style="width:30%">
+    //        <small class="badge bg-label-warning dataedit" style="cursor:pointer;">編輯</small>
+    //        <small class="badge bg-label-danger datadel" style="cursor:pointer;">刪除</small>
+    //      </td>
+    //      </tr>`)
+    //    });
+    //  }
   }
   $(`#mainwait`).remove();
   $(`#ddatetable`).css("display","");
