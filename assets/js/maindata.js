1043fba895214cf7184f5ff932022834b39565fe
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2023-12-11 11:12:29 +0800

    background url

--- assets/js/maindata.js
+++ assets/js/maindata.js
@@ -1,4 +1,4 @@
-var url = "https://0185-124-218-106-67.ngrok-free.app";
+var url = "https://e198-1-162-39-36.ngrok-free.app";
 //var url = "https://localhost:7226";
 var fronturl = "https://alan-ddddd.github.io/JD/html";
 var datalist;
