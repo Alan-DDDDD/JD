0dd0c5e6c686ba3397525b9d08621c5e2fbb6dde
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2023-12-18 11:29:17 +0800

    1

--- assets/js/case.js
+++ assets/js/case.js
@@ -17,7 +17,7 @@ $(async function(){
     headers: new Headers({
       "ngrok-skip-browser-warning": "69420",
     }),
-    body : JSON.stringify(["BS","SS","CMD","CL","CS"])
+    body : JSON.stringify({parames:["BS","SS","CMD","CL","CS"]})
   })
   var carddldata = await carddl.json();
   if(carddldata.Status){
