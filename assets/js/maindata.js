95329de37bd2a91121640c33e9acebe1b0434692
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2023-12-21 10:57:25 +0800

    1

--- assets/js/maindata.js
+++ assets/js/maindata.js
@@ -7,31 +7,7 @@ var curruntuser;
 var curruntid;
 var curruntlevel;
 
-function response(result){
-    if(result.Status){
-        if(result.error){//資料邏輯錯誤
-          alert("");
-        }else{
-          alert("新增成功!!");
-          return true;
-        }
-      }else{//系統錯誤
-        alert("連線失敗!!");
-      }
-}
 
-async function ajax(controller,data,method){
-    var response = await fetch(url + controller + curruntid,{
-        method : method,
-        headers : new Headers({
-          "ngrok-skip-browser-warning": "69420",
-          "Content-Type":"application/json"
-        }),
-        body : JSON.stringify(data)
-      });
-      var body = await response.json();
-      return body;
-}
 
 Number.prototype.numberFormat = function(c, d, t){
   var n = this, 
