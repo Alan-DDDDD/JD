fd19b9d16bec9248273c6f03b39eae55df7034f0
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2024-01-04 09:41:35 +0800

    url

--- assets/js/maindata.js
+++ assets/js/maindata.js
@@ -1,4 +1,4 @@
-var url = "https://67b0-1-162-57-153.ngrok-free.app";
+var url = "https://6ccf-1-162-57-153.ngrok-free.app";
 //var url = "https://localhost:5002";
 var fronturl = "https://alan-ddddd.github.io/JD/html";
 var datalist;
